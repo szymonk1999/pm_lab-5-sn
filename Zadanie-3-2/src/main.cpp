74a0f210bc84a78e2206c053f197d01d12af157a
„Szymon <szymonk@onet.pl>
2021-01-25 09:17:24 +0100

    Wykonano zadanie 3.2.1 –inicjalizacja LCD

--- Zadanie-3-2/src/main.cpp
+++ Zadanie-3-2/src/main.cpp
@@ -1,9 +1,34 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
 
-void setup() {
-  // put your setup code here, to run once:
+
+LiquidCrystal lcd(9, 8, 7, 6, 5, 4);
+int menu=1;
+
+void setup(void) {
+  lcd.begin(16, 2);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(void) {
+  dispMenu();
+  delay(1000);
+  menu++;
+  if(menu>3) menu=1; 
+}
+
+  void dispMenu(void){
+    switch(menu){
+      case 1:
+        lcd.setCursor(0,0);
+        lcd.print("Menu 1");
+        break;
+      case 2:
+        lcd.setCursor(0,0);
+        lcd.print("Menu 2");
+        break;
+      case 3:
+        lcd.setCursor(0,0);
+        lcd.print("Menu 3");
+        break;
+  }
 }
